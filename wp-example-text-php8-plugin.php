c899da7f8aac8f03f67ea6f467114c84fb7325b0
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-09-30 09:34:54 -0400

    Add stub main plugin file

---
+++ wp-example-text-php8-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Example Test PHP8 Plugin
+ * Plugin URI:        https://github.com/salcode/wp-example-test-php8-plugin
+ * Description:       An example plugin for testing for PHP8 compatibility, specifically with code incompatible with PHP8
+ * Version:           0.1.0
+ * Requires at least: 6.0.0
+ * Requires PHP:      7.4
+ * Author:            Sal Ferrarello
+ * Author URI:        https://salferrarello.com/
+ * License:           MIT
+ * License URI:       https://mit-license.org/
+ * Text Domain:       wp-example-test-php8-plugin
+ * Domain Path:       /languages
+ */
+
+namespace salcode\WpExampleTestPhp8;
